ce7c403362eb47bcbac068aa7a51227d851ea834
Michael Dougherty <maackle.d@gmail.com>
2019-03-11 18:26:48 -0700

    Starting on test rewrite

--- test/common.ts
+++ test/common.ts
@@ -3,12 +3,6 @@ import {Client as RpcClient, Server as RpcServer} from 'rpc-websockets'
 
 import {IntrceptrServer} from '../src/server'
 
-export const testClient = () => sinon.stub(new RpcClient())
+export const testRpcClient = () => sinon.stub(new RpcClient())
 
-const testRpcServer = () => sinon.stub(new RpcServer({noServer: true}))
-
-export const testServer = () => new IntrceptrServer({
-  server: testRpcServer(), 
-  adminClient: testClient(),
-  happClient: testClient(),
-})
+export const testRpcServer = () => sinon.stub(new RpcServer({noServer: true}))
--- test/test-zome-call.ts
+++ test/test-zome-call.ts
@@ -1,4 +1,26 @@
 import * as test from 'tape'
 import * as sinon from 'sinon'
 
-import {testClient, testServer} from './common'
+import {testRpcClient, testRpcServer} from './common'
+import {IntrceptrServer} from '../src/server'
+
+const setup = () => {
+  const masterClient = testRpcClient()
+  const publicClient = testRpcClient()
+  const internalClient = testRpcClient()
+  const intrceptr = new IntrceptrServer({
+    masterClient: testRpcClient(),
+    publicClient: testRpcClient(),
+    internalClient: testRpcClient(),
+  })
+  intrceptr.server = testRpcServer()
+  return {intrceptr, masterClient, publicClient, internalClient}
+}
+
+test('can call public zome function', t => {
+  const {intrceptr, masterClient, publicClient, internalClient} = setup()
+  
+
+
+  t.end()
+})
