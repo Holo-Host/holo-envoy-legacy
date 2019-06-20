62da0b9a21a6cc5368946d8c158467937926dc6f
Michael Dougherty <maackle.d@gmail.com>
2019-06-20 10:31:07 -0700

    Typo

--- src/common.ts
+++ src/common.ts
@@ -153,7 +153,7 @@ export const zomeCallSpec = ({zomeName, funcName}) => (
  * Make a zome call through the WS client, identified by instance ID
  * TODO: maybe keep the Ok/Err wrapping, to differentiate between zome error and true exception
  */
-export const zomeCallByInstance = async (client, {instanceId, zomeName, funcName, params = {}) => {
+export const zomeCallByInstance = async (client, {instanceId, zomeName, funcName, params = {}}) => {
   const payload = {
     instance_id: instanceId,
     zome: zomeName,
