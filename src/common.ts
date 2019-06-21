581572b8cc204444ec21a7285006e80c14317f9c
Lisa Jetton <ldjetton21@gmail.com>
2019-06-20 19:15:11 -0500

    Update src/common.ts
    Good suggestion. I think this is a good practice to put in motion.
    
    Co-Authored-By: Daniel Szymanek <danekszy@users.noreply.github.com>

--- src/common.ts
+++ src/common.ts
@@ -162,7 +162,7 @@ type CallFnParams = {
  * TODO: maybe keep the Ok/Err wrapping, to differentiate between zome error and true exception
  */
 export const zomeCallByInstance = async (client, callParams: CallFnParams) => {
-  const {instanceId, zomeName, funcName, args} = callParams
+  const {instanceId, zomeName, funcName, args = {}} = callParams
   const payload = {
     instance_id: instanceId,
     zome: zomeName,
