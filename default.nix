30956584391e3e19ac27c9a3875b6dd9a7027c63
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-27 14:00:16 +0000

    Add basic Nix build/shell

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import ./pkgs.nix {} }:
+
+with pkgs;
+
+{
+  holo-envoy = stdenv.mkDerivation rec {
+    name = "holo-envoy";
+    src = gitignoreSource ./.;
+    
+    nativeBuildInputs = [
+      holochain-cli
+      holochain-conductor
+      nodejs
+    ];
+    
+    preConfigure = ''
+      cp -Lr ${npmToNix { inherit src; }} node_modules
+      chmod -R +w node_modules
+      patchShebangs node_modules
+    '';
+    
+    buildPhase = ''
+      node_modules/typescript/bin/tsc -d
+    '';
+    
+    installPhase = ''
+      mkdir $out
+      mv * $out
+    '';
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,1 @@
+(import ./. {}).holo-envoy
